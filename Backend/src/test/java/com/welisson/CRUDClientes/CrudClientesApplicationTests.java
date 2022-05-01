9923341a9e9c594209716014712e888129a7b452
welissom <welissomlima@hotmail.com>
2022-05-01 19:21:45 -0300

    CRUD client created

---
+++ Backend/src/test/java/com/welisson/CRUDClientes/CrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welisson.CRUDClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
