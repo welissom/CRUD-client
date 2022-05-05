975f7f8ad19fc06274e431565dfa6a32412bd207
welissom <welissomlima@hotmail.com>
2022-05-04 22:19:19 -0300

    Pagination

--- Backend/src/main/java/com/welisson/CRUDClientes/services/ClientService.java
+++ Backend/src/main/java/com/welisson/CRUDClientes/services/ClientService.java
@@ -1,14 +1,14 @@
 package com.welisson.CRUDClientes.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,10 +26,9 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll(){
-		List<Client> list = repository.findAll();
-		
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+		Page<Client> list = repository.findAll(pageRequest);		
+		return list.map(x -> new ClientDTO(x));
 	}
 
 	@Transactional(readOnly = true)
@@ -83,4 +82,5 @@ public class ClientService {
 		}
 		
 	}
+	
 }
